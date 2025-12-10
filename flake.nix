75643cd6233f97646b25b5ee36c692a48a23c1e8
nacl-gb3 <67475811+nacl-gb3@users.noreply.github.com>
2025-12-09 20:59:30 -0600

    Setup dev environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.go
+            pkgs.pkg-config
+            pkgs.flex
+            pkgs.bison
+            pkgs.libuuid
+            pkgs.libarchive
+            (pkgs.python3.withPackages (python-pkgs: [ ]))
+          ];
+          buildInputs = [ ];
+        };
+      }
+    );
+}
